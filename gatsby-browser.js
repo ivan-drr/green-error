65e6c66a7dce4a109cdd0dc652cca597b97557cd
ivan-drr <ivan.delrr@gmail.com>
2020-01-17 17:33:32 +0000

    skip Icons dependency of UIkit update gitignore

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,4 @@
-import UIkit from "uikit"
-import Icons from "uikit/dist/js/uikit-icons"
-
+require("uikit")
 require("./src/styles/global.css")
 require("./node_modules/uikit/dist/css/uikit.min.css")
 require("prismjs/themes/prism-tomorrow.css")
-
-UIkit.use(Icons)
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
