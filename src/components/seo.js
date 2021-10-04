19414a46051d8ee962c22c02e6af4f2c26f3c44b
Iván Del Río Ruger <43607995+ivan-drr@users.noreply.github.com>
2021-10-04 19:21:37 +0200

    update featured image

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import featured_image from '../images/featured_image.png'
+import { featured_image } from '../images/featured_image.png'
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
